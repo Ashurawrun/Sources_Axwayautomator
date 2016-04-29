44bd6c63a8852805d12e3b8a47fdf8a935efa3c7
Zachary <zachary.aich@gmail.com>
2016-04-29 14:19:52 +0100

    Logs of testings + .bat file to create scripts faster

---
+++ Migration.bat
@@ -0,0 +1,13 @@
+cd /D C:\Users\zachary\Desktop\Sources_Axwayautomator\sources\
+python Crea_cal.py
+python Crea_User.py
+python Crea_Agents.py
+python Crea_Queue.py
+python Crea_env.py
+python Crea_Ressources.py
+python Crea_App.py
+python Crea_Job.py
+python Crea_Link.py
+cd /D C:\Users\zachary\Desktop\Sources_Axwayautomator\resultats\
+echo "executer les scripts?"
+pause
